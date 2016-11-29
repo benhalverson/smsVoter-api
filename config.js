d67b5e24323cfbf604cbf5423ff3ba5ff4e789ef
Ben Halverson <benhalverson33@gmail.com>
2016-11-29 13:46:35 -0800

    changed info on config.js

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ config.twilio = {};
 
 config.couchdb.url = 'couchdb://localhost/smsData';
 config.couchdb.port = 5984;
-config.couchdb.username = 'admin';
-config.couchdb.password = 'apple123';
+config.couchdb.username = process.env.couchdb.username || 'admin';
+config.couchdb.password = process.env.couchdb.password || 'apple123';
 
-config.twilio.sid = 'ACxxx';
-config.twilio.key = 'yyy';
-config.twilio.smsWebhook = 'http://localhost/vote/sms';
-config.twilio.voiceWebhook = 'http://localhost/vote/voice';
+config.twilio.sid = process.env.twilio.sid || 'ACxxx';
+config.twilio.key = process.env.twilio.key || 'yyy';
+config.twilio.smsWebhook = process.env.twilio.smsWebhook || 'http://localhost/vote/sms';
+config.twilio.voiceWebhook = process.env.twilio.voiceWebhook || 'http://localhost/vote/voice';
 config.disableTwilioSigCheck = false;
 module.exports = config;
